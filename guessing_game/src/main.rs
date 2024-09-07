ffa4877313315389087ec2ad58f61e378ae14b9a
dev-frog <kali.nisga@gmail.com>
2024-09-08 03:36:53 +0600

    chore: Update dependencies for guessing game project

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess The Number!");
+
+    loop {
+        let secret_number: i32 = rand::thread_rng().gen_range(1, 101);
+
+        println!("Please input your guess.");
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+        println!("Secret number is: {}", secret_number);
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}","Too small!".red()),
+            Ordering::Greater => println!("{}","Too Big".red()),
+            Ordering::Equal => {
+                println!("{}","You Win!".green());
+                break;
+            }
+        }
+
+    }
 }
